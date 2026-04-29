8712802c4b05cd9745b4087877acc39ca87d143f
Odd-Harald <oddharald@myhren.ai>
2026-04-29 12:21:10 +0300

    Trying to get CUDA working but gets stuck

--- flake.nix
+++ flake.nix
@@ -7,17 +7,42 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        cuda = pkgs.cudaPackages_13;
+
+        onnxruntime-gpu = pkgs.fetchurl {
+          url = "https://github.com/microsoft/onnxruntime/releases/download/v1.25.1/onnxruntime-linux-x64-gpu_cuda13-1.25.1.tgz";
+          hash = "sha256-68FOEpDbKjCnu0Fb0cPhOQp4FrtNuHZ33DbQce0igzw=";
+        };
+
+        ort-lib = pkgs.runCommand "ort-lib-1.25.1" {} ''
+          mkdir -p $out/lib
+          tar xzf ${onnxruntime-gpu} --strip-components=2 -C $out/lib \
+            onnxruntime-linux-x64-gpu-1.25.1/lib/
+        '';
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             pkg-config
             alsa-lib
             openssl
+            cuda.cudatoolkit
+            cuda.cudnn
           ];
 
           LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [ alsa-lib ]);
+          CUDA_HOME = "${cuda.cudatoolkit}";
+          ORT_DYLIB_PATH = "${ort-lib}/lib/libonnxruntime.so";
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.alsa-lib
+            cuda.cudatoolkit
+            cuda.cudnn.lib
+            "${ort-lib}"
+            "/run/opengl-driver"
+          ];
         };
       });
 }
